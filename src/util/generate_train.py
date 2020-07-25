ed3b264f1344f212dd99762419b34d5a929cd098
double Z <doubleZ0108@163.com>
2020-07-25 19:27:03 +0800

    update data model

--- src/util/generate_train.py
+++ src/util/generate_train.py
@@ -5,10 +5,10 @@
 import os
 
 image_files = []
-os.chdir("../data/img")
+os.chdir("data/img/")
 for filename in os.listdir(os.getcwd()):
     if not filename.endswith(".txt") and filename != ".DS_Store":
-        image_files.append("data/img/" + filename)
+        image_files.append("../../data/img/" + filename)
 os.chdir("..")
 
 with open("train.txt", "w") as outfile:
